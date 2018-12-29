b07a388d561a5071fe33aef21f23db316f8c568a
Will-Guthrie <william.guthrie@mail.mcgill.ca>
2018-12-29 00:23:23 -0600

    Add basic non calculated stats

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,12 @@
 public class Player {
+
+    private int level;
+    private int strength;
+    private int dexterity;
+    private int constitution;
+    private int intelligence;
+    private int wisdom;
+    private int charisma;
+    private int hitPointsMax;
+
 }
